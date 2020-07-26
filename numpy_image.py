913ac0744874bf7eb4d779857635429b11c8c11d
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-26 20:54:29 +0530

    Created A White Image

--- numpy_image.py
+++ numpy_image.py
@@ -1,8 +1,9 @@
 import numpy as np
 import cv2
 
-black = np.zeros([150,200,1],'uint8')
-cv2.imshow("Black",black)
+white = np.ones([150,200,1],'uint8')
+white =  white *  ( 2**8 - 1 )
+cv2.imshow("White",white)
 
 cv2.waitKey(0)
 
